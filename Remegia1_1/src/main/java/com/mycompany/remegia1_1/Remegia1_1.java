370463691db4ebcb5e78947776715fb5f69c12c3
HumbaAdob0 <148634828+HumbaAdob0@users.noreply.github.com>
2024-02-13 21:09:57 +0800

    Add files via upload

---
+++ Remegia1_1/src/main/java/com/mycompany/remegia1_1/Remegia1_1.java
@@ -0,0 +1,36 @@
+package com.mycompany.remegia1_1;
+
+import java.util.Scanner;
+
+public class Remegia1_1 {
+static Scanner str = new Scanner(System.in);
+    static Scanner num = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    
+
+        System.out.print("Enter name: ");
+        String name = str.nextLine();
+        
+        System.out.print("Enter age: ");
+        int age = num.nextInt();
+        
+        System.out.print("Enter gender: ");
+        String gen = str.nextLine();
+        
+        System.out.print("Enter address: ");
+        String addr = str.nextLine();
+        
+        System.out.print("Enter civil status: ");
+        String CS = str.nextLine();
+        
+        System.out.print("Enter no. siblings: ");
+        int nsib = num.nextInt();
+        
+        
+        
+        
+        
+    }
+    
+}
