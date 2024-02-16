cb60bc1fe2ac61ece08dbe746a6b1b45211e88ce
HumbaAdob0 <148634828+HumbaAdob0@users.noreply.github.com>
2024-02-16 17:27:40 +0800

    Lab Activity 1.2

---
+++ Remegia1_1/src/Main/main.java
@@ -0,0 +1,33 @@
+
+package Main;
+
+import java.util.Scanner;
+
+public class main {
+    
+    static Scanner str = new Scanner(System.in);
+    static Scanner num = new Scanner(System.in);
+    
+    public static void main(String[] args){
+        
+        System.out.print("Enter name: ");
+            String name = str.nextLine();
+        
+            System.out.print("Enter age: ");
+            int age = num.nextInt();
+        
+            System.out.print("Enter gender: ");
+            
+            String gen = str.nextLine();
+        
+            System.out.print("Enter address: ");
+            String addr = str.nextLine();
+        
+            System.out.print("Enter civil status: ");
+            String CS = str.nextLine();
+        
+            System.out.print("Enter no. siblings: ");
+             int nsib = num.nextInt();
+    
+    }
+}
